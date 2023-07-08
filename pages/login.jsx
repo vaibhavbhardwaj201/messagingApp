942de607972eef977d31655e18b5e01dc4b7a6ff
Vaibhav Bhardwaj <vaibhavbhardwaj1230@gmail.com>
2023-07-08 22:23:39 +0300

    Add login and register page

---
+++ pages/login.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='min-h-screen flex justify-center items-center bg-c1'>
+        <div className="flex items-center flex-col">
+          <div className="text-center">
+            <div className="text-4xl text-bold">
+              Login to your account
+            </div>
+            <div className="mt-3 text-c3">
+              Chat with someone you like...
+            </div>
+          </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Login
---
+++ pages/register.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+
+    <div className='min-h-screen flex justify-center items-center bg-c1'>
+        <div className="flex items-center flex-col">
+          <div className="text-center">
+            <div className="text-4xl text-bold">
+              register
+            </div>
+          </div>
+        </div>
+        
+    </div>
+
+  )
+}
+
+export default Register
